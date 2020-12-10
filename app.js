09c6f71e43704a99853b86315f4cfa74ee3d89bb
sherinoctavani <Sherinoktavani@gmail.com>
2020-12-10 08:42:34 -0800

    nambahin Find All Question and Answer

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
 const cors = require('cors') 
+const {Question, Answer} = require ("./models")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
@@ -17,6 +18,21 @@ io.on('connection', (socket) => {
     socket.on('login', (user) => {
         userLogin.push(user)
         io.emit('userLogin', userLogin)
+    })
+})
+
+app.get('/quiz', (req, res) => {
+    let dataAnswers = {
+        include : {
+            model: Answer
+        }
+    }
+    Question.findAll(dataAnswers)
+    .then(data => {
+        res.status(200).json(data)
+    })
+    .catch(err => {
+        res.status(err.status).json(err)
     })
 })
 
